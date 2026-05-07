ade5f15ffe9bbde2a365af1933c0c8bad46746a2
Debora <deboratanowe.aguiar@gmail.com>
2026-05-07 19:35:47 -0300

    alteracao na classe VacinaService

--- thermvaccine/src/main/java/com/thermvaccine/service/VacinaService.java
+++ thermvaccine/src/main/java/com/thermvaccine/service/VacinaService.java
@@ -3,17 +3,11 @@ import com.thermvaccine.model.Vacina;
 
 public class VacinaService {
 
-    private Vacina vacina;
-
-    public VacinaService(Vacina vacina){
-        this.vacina = vacina;
-    }
-
     void exibirDados(Vacina vacina){
         //System.out.printf("ID: %d\n", this.vacina.getId());
-        System.out.printf("Nome: %s\n", this.vacina.getNome());
-        System.out.printf("Temperatura mínima suportada: %f\n", this.vacina.getTempe_min());
-        System.out.printf("Temperatura máxima suportada: %f\n", this.vacina.getTempe_max());
+        System.out.printf("Nome: %s\n", vacina.getNome());
+        System.out.printf("Temperatura mínima suportada: %f\n", vacina.getTempe_min());
+        System.out.printf("Temperatura máxima suportada: %f\n", vacina.getTempe_max());
 
     }
 }
